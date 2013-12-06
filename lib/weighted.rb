09e8db00fdb984e5633fac017fe2c0f78828d31e
Thomas Wolfe <tomwolfe@gmail.com>
2013-12-06 04:41:35 -0600

    multiple assignment, return value not puts, duplicate methods (just call initialize)

--- lib/weighted.rb
+++ lib/weighted.rb
@@ -8,24 +8,21 @@ class Weighted
 	end
 
 	def items=(items)
-		@items = items
-		generate_intervals_sum_weights
+		initialize(items)
 	end
 
 	def pick(count = 1)
-		return_vals = []
-		return_val = 0
+		return_vals, return_val = [], 0
 		count.times do
 			r = rand(1..@sum_weights)
 			@intervals.keys.each { |key| return_val = @intervals[key] if r >= key }
 			return_vals << return_val
 		end
-		puts return_vals
+		return_vals
 	end
 
 	def generate_intervals_sum_weights
-		@sum_weights = 0
-		@intervals = {}
+		@sum_weights, @intervals = 0, {}
 		@items.each_pair do |key, value|
 			@sum_weights += value
 			@intervals[@sum_weights] = key
